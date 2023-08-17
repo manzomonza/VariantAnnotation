1e7655f63c566b9ab89f9cbacd38550c8b51bad5
manzomonza <massimiliano.manzo@posteo.ch>
2023-08-17 11:20:38 +0200

    Add filepath function documentation

--- call_AnnotationModules.R
+++ call_AnnotationModules.R
@@ -4,6 +4,6 @@
 ## -- snvt
 
 snvt = readr::read_tsv(parsed_fp$parsed_snv)
-annotation_fp = annotation_filepaths(analysis_dir)
+annotation_fp = VariantAnnotationModulesannotation_filepaths(analysis_dir)
 write_Annotation_Modules(snvt, annotation_fp = annotation_fp)
 
