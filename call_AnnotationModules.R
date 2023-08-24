11f6322fe8bc46391708e78fbf6db167a1685253
manzomonza <massimiliano.manzo@posteo.ch>
2023-08-24 12:19:18 +0200

    Include Horak function call in call_AnnotationModules

--- call_AnnotationModules.R
+++ call_AnnotationModules.R
@@ -7,7 +7,7 @@ snvt = readr::read_tsv(parsed_fp$parsed_snv)
 annotation_fp = VariantAnnotationModules::annotation_filepaths(analysis_dir)
 write_Annotation_Modules(snvt, annotation_fp = annotation_fp)
 
-# horak_table = "/Users/manzo/USB/USB_Diagnostics/VariantAnnotationModules/HorakScoreTable.txt"
+horak_table = "~/github_app/VariantAnnotationModules/HorakScoreTable.txt"
 #
 # testpath = '/Users/manzo/USB/USB_Diagnostics/ShinyVariants/testfiles'
 # df = data.frame(paths = list.files(path = testpath, pattern = "annotation_", recursive = TRUE, full.names = TRUE))
@@ -16,9 +16,9 @@ write_Annotation_Modules(snvt, annotation_fp = annotation_fp)
 # df=tibble::as_tibble(df[,c(2,1)])
 # df = dplyr::group_by(df, sample)
 # df = dplyr::group_split(df)
-# (horak_scores = Horak_score_function_calls(df[[7]]$paths))
-# HorakScore(horak_scores)
-# Horak_classification()
-#
-# Horak_vals = HorakScore(horak_scores)
-# Horak_vals$classification = sapply(Horak_vals$Horak_score, Horak_classification)
+(horak_scores = Horak_score_function_calls(annotation_fp$paths))
+
+Horak_vals = HorakScore(horak_scores)
+Horak_vals$classification = sapply(Horak_vals$Horak_score, Horak_classification)
+
+readr::write_tsv(Horak_vals, file=annotation_fp$Horak)
