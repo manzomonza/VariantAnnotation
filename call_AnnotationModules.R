4f54c1c1767c568390783d6f2ccbf05e9c1e5739
manzomonza <massimiliano.manzo@posteo.ch>
2023-10-11 16:11:46 +0200

    Add BRAF check and Gauth call

--- call_AnnotationModules.R
+++ call_AnnotationModules.R
@@ -16,7 +16,7 @@ annotation_fp = VariantAnnotationModules::annotation_filepaths(analysis_dir)
 write_Annotation_Modules(snvt, annotation_fp = annotation_fp)
 
 ### MP Variant Table
-source("/home/ionadmin/github/GDrive_VariantReport/Gauths.R")
+source("/home/ionadmin/github_app/GDrive_VariantReport/Gauths.R")
 idoi = googledrive::as_id("https://docs.google.com/spreadsheets/d/1B-NfpRNhadl7w1f5UPkRA_XEg4YI3N4pHRxd9yZgZkc/edit?usp=drive_web&ouid=116704210424700639172")
 MPvars = googlesheets4::read_sheet(idoi, skip = 1)
 mpvs = MP_check_retrieve_table(snv, MPvars)
@@ -29,7 +29,7 @@ if(nrow(mpvs) > 0){
 ### BRAF Variant Class
 idoi = googledrive::as_id("https://docs.google.com/spreadsheets/d/1xQ3FfHV2JLndT7J_yLHGcrb7Uqpc-cjjio9OYXkjz14/edit")
 MPvars = googlesheets4::read_sheet(idoi, skip = 1)
-mpvs = BRAF_class_retrieve_table(snv, MPvars)
+mpvs = BRAF_class_retrieve_table(snvt, MPvars)
 braf_class = paste0(analysis_dir,'/annotation_output/annotation_BRAF_variant_class.tsv')
 if(nrow(mpvs) > 0){
   readr::write_tsv(mpvs, file = braf_class)
