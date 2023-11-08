08951380c568b9e79722069cdce2432fadd95ab8
manzomonza <massimiliano.manzo@posteo.ch>
2023-11-08 15:29:06 +0100

    Add function to highlight TSG CNV loss

--- R/annotate_CNVs.R
+++ R/annotate_CNVs.R
@@ -13,7 +13,7 @@
 #' @export
 #'
 #' @examples
-annotate_cnv = function(cnv_table, cnv_lut){
+annotate_cnv = function(cnv_table, cnv_lut, tsg_list){
   cnv_rows = 1:nrow(cnv_table)
   cnv_table$cnv_status = unlist(lapply(cnv_rows, function(i) determine_cnv_status(perc_05 = cnv_table$perc_0.05[i],
                                                                                   perc_95 = cnv_table$perc_0.95[i])))
@@ -21,6 +21,10 @@ annotate_cnv = function(cnv_table, cnv_lut){
   cnv_table$cnv_effect = unlist(lapply(cnv_rows, function(i) gene_cnv_effect(gene = cnv_table$gene[i],
                                                                              cnv_status = cnv_table$cnv_status[i],
                                                                              cnv_lut = cnv_lut)))
+  cnv_table$TSG_cnv = unlist(lapply(cnv_rows, function(i) tsg_cnv_effect(gene = cnv_table$gene[i],
+                                                                             cnv_status = cnv_table$cnv_status[i],
+                                                                         tsg_list = tsg_list)))
+
   return(cnv_table)
 }
 
@@ -67,5 +71,23 @@ gene_cnv_effect = function(gene, cnv_status, cnv_lut){
   }
 }
 
+#' Determine if CNV loss is happening in TSG
+#'
+#' @param gene
+#' @param cnv_status
+#' @param tsg_list
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tsg_cnv_effect = function(gene, cnv_status, tsg_list){
+  tsggene = gene %in% names(tsg_list)
+  if(tsggene){
+    if(cnv_status == "loss"){
+      return("likely pathogenic")
+    }
+  }
+}
 
 
