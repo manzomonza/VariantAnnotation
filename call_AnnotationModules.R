6709c18f4839dc5acad6e32470aa37fdf42563a9
Alla Shundrovsky <alla.shundrovsky@thermofisher.com>
2023-11-13 17:48:08 +0100

    Changed callModule to not provide GoogleSheet links

--- call_AnnotationModules.R
+++ call_AnnotationModules.R
@@ -17,8 +17,8 @@ write_Annotation_Modules(snvt, annotation_fp = annotation_fp)
 
 ### MP Variant Table
 source("/home/ionadmin/github_app/GDrive_VariantReport/Gauths.R")
-idoi = googledrive::as_id("https://docs.google.com/spreadsheets/d/1B-NfpRNhadl7w1f5UPkRA_XEg4YI3N4pHRxd9yZgZkc/edit?usp=drive_web&ouid=116704210424700639172")
-MPvars = googlesheets4::read_sheet(idoi, skip = 1)
+# mpvarlink = googledrive::as_id("https://docs.google.com/spreadsheets/d/1B-NfpRNhadl7w1f5UPkRA_XEg4YI3N4pHRxd9yZgZkc/edit?usp=drive_web&ouid=116704210424700639172")
+MPvars = googlesheets4::read_sheet(mpvarlink, skip = 1)
 mpvs = MP_check_retrieve_table(snvt, MPvars)
 mpv_filepath = paste0(analysis_dir,'/annotation_output/annotation_MP_variant.tsv')
 if(nrow(mpvs) > 0){
@@ -27,21 +27,21 @@ if(nrow(mpvs) > 0){
 
 
 ### BRAF Variant Class
-idoi = googledrive::as_id("https://docs.google.com/spreadsheets/d/1xQ3FfHV2JLndT7J_yLHGcrb7Uqpc-cjjio9OYXkjz14/edit")
-MPvars = googlesheets4::read_sheet(idoi, skip = 1)
-mpvs = BRAF_class_retrieve_table(snvt, MPvars)
+# braflink = googledrive::as_id("https://docs.google.com/spreadsheets/d/1xQ3FfHV2JLndT7J_yLHGcrb7Uqpc-cjjio9OYXkjz14/edit")
+BRAFinfo = googlesheets4::read_sheet(braflink, skip = 1)
+brafi = BRAF_class_retrieve_table(snvt, BRAFinfo)
 braf_class = paste0(analysis_dir,'/annotation_output/annotation_BRAF_variant_class.tsv')
-if(nrow(mpvs) > 0){
-  readr::write_tsv(mpvs, file = braf_class)
+if(nrow(brafi) > 0){
+  readr::write_tsv(brafi, file = braf_class)
 }
 
 ### HRR genes
-idoi = googledrive::as_id('https://docs.google.com/spreadsheets/d/11FZz5m34IYmK1-o2UkjFJ7gatXUBr4_F0mhVdGMxQm8/edit#gid=230169235')
-MPvars = googlesheets4::read_sheet(ss = idoi, sheet  = "HRR_single_genes", skip = 1)
-mpvs = HRR_check_retrieve_table(snvt, MPvars)
-HRRgenes = paste0(analysis_dir,'/annotation_output/annotation_HRRgenes.tsv')
+# hrrgenes = googledrive::as_id('https://docs.google.com/spreadsheets/d/11FZz5m34IYmK1-o2UkjFJ7gatXUBr4_F0mhVdGMxQm8/edit#gid=230169235')
+HRRgenes = googlesheets4::read_sheet(ss = hrrgenes, sheet  = "HRR_single_genes", skip = 1)
+hrrs = HRR_check_retrieve_table(snvt, HRRgenes)
+HRRgenepath = paste0(analysis_dir,'/annotation_output/annotation_HRRgenes.tsv')
 if(nrow(mpvs) > 0){
-  readr::write_tsv(mpvs, file = HRRgenes)
+  readr::write_tsv(hrrs, file = HRRgenepath)
 }
 
 
